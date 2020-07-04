c08b626553db8a2e9b07e0b8e1fad873a9a53c96
Василий Щербаков <v.sherbakov@softpro.com>
2020-07-04 18:00:42 +0300

    add swagger

--- backend/src/main/kotlin/org/mai/dep810/sandbox/controller/LeadersController.kt
+++ backend/src/main/kotlin/org/mai/dep810/sandbox/controller/LeadersController.kt
@@ -2,9 +2,16 @@ package org.mai.dep810.sandbox.controller
 
 import org.mai.dep810.sandbox.service.PlayerService
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
 import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
+import springfox.documentation.builders.PathSelectors
+import springfox.documentation.builders.RequestHandlerSelectors
+import springfox.documentation.spi.DocumentationType
+import springfox.documentation.spring.web.plugins.Docket
+import springfox.documentation.swagger2.annotations.EnableSwagger2
 import kotlin.streams.toList
 
 @RestController
@@ -19,5 +26,18 @@ class LeadersController {
                          .stream()
                          .map { it.handle }
                          .toList()
+}
+
+@Configuration
+@EnableSwagger2
+class SwaggerConfig {
+    @Bean
+    fun api(): Docket {
+        return Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+    }
 }
 
