52e08723209444651bebef8f48c2c878bddfded7
Василий Щербаков <v.sherbakov@softpro.com>
2020-07-04 16:33:05 +0300

    refactoring project for using fullstack in intelij + using dotnenv for backend url

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom'
 const App = () => {
   const getLeaders = event =>{
     const axios = require('axios');
-    axios.get('http://localhost:8080/leaders')
+    axios.get(`${process.env.REACT_APP_LEADERS_API_URL}/leaders`)
          .then(resp =>{
           console.log(resp.data);
           ReactDOM.render(<ol>{resp.data.map(leader => <li>{leader}</li>)}</ol>
