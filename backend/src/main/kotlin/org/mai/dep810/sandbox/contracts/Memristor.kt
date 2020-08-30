2a8d85f596ede36706b17f81b933bb2d78f6f1cb
Василий Щербаков <v.sherbakov@softpro.com>
2020-08-30 09:09:35 +0300

    feature-backend: add contracts for memristor, neuron

---
+++ backend/src/main/kotlin/org/mai/dep810/sandbox/contracts/Memristor.kt
@@ -0,0 +1,14 @@
+package org.mai.dep810.sandbox.contracts
+
+data class Memristor(
+        var id: Int,
+        var params: List<MemristorParams>,
+        var description: String
+)
+
+data class MemristorParams(
+        var up: Double,
+        var down: Double,
+        var value: Double,
+        var name: String
+)
