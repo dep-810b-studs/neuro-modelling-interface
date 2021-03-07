eaf41e4b25cddb58b2586671a346cdb8f7698f8c
sh-vasily <vasia.punk.rok@gmail.com>
2021-03-07 21:03:36 +0300

    ignore webpack error

--- ui/src/Components/MemristorParams.tsx
+++ ui/src/Components/MemristorParams.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import info from "./../ConstData/info.json";
+import './MemristorParams.css';
+import info from "./../const_data/info.json";
 
 const modelMemristors = info.model_memristors;
 
 export const MemristorParams: React.FunctionComponent = () => (
-   <ul>
+   <ul className="memrisotor-params-list">
        {modelMemristors
            .map(model => (
                <li>
--- ui/src/Pages/Home.tsx
+++ ui/src/Pages/Home.tsx
@@ -1,10 +1,15 @@
 import React from "react";
+import './Home.css';
 import MemristorParams from "../Components/MemristorParams";
+import NetworkParams from "../Components/NetworkParams";
+import Patterns from "../Components/Patterns";
 
 function Home() {
   return (
-    <div className='home'>
+    <div className='home-page-wrapper'>
         <MemristorParams/>
+        <NetworkParams/>
+        <Patterns/>
     </div>
   );
 }
