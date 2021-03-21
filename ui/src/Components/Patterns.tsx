9f83d07371aaec63fc3982bd0cfa1aa923e978d7
sh-vasily <vasia.punk.rok@gmail.com>
2021-03-21 12:44:53 +0300

    use component for PatternImage

--- ui/src/Components/Patterns.tsx
+++ ui/src/Components/Patterns.tsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import './Patterns.css';
 import info from "./../const_data/info.json";
+import ImagePattern from "./ImagePattern";
 
 const patterns = info.patterns;
 
 export const Patterns: React.FunctionComponent = () => (
-    <ul className="patterns">
-        {patterns
-            .map(pattern => (
-                <li>
-                    {pattern}
-                </li>
-            ))}
-    </ul>
+    <div className="patterns-wrapper">
+        <h1>
+            Шаблоны для распознавания
+        </h1>
+        <div className="patterns">
+            {patterns.map(pattern => (<ImagePattern Values={pattern}/>))}
+        </div>
+    </div>
 );
 
 export default Patterns;
