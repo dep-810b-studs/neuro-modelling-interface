4c130a4d5bc63f6fec3879815d846b1bf219b900
sh-vasily <vasia.punk.rok@gmail.com>
2021-03-21 13:54:11 +0300

    add header

--- ui/src/Components/NetworkParams.tsx
+++ ui/src/Components/NetworkParams.tsx
@@ -6,6 +6,9 @@ const networkParams = info.neural_network;
 
 export const NetworkParams: React.FunctionComponent = () => (
     <div className="nertwork-params-list">
+        <h3>
+            Параметры нейросети
+        </h3>
         {networkParams.description}
     </div>
 );
