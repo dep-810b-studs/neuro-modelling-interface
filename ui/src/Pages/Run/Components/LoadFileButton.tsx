1bf53f92e65f97e7b35ce6a99e00f6faa06fe00c
sh-vasily <vasia.punk.rok@gmail.com>
2021-05-30 15:28:11 +0300

    add component for loading run task input

--- ui/src/Pages/Run/Components/LoadFileButton.tsx
+++ ui/src/Pages/Run/Components/LoadFileButton.tsx
@@ -1,12 +1,24 @@
+import React from 'react';
+import { Button } from 'semantic-ui-react';
 import { stateManager } from '../../..';
 import './LoadFileButton.css';
+import "./../RunTasks.css";
 
-const LoadFileButton = () =>  
-    <input 
-        type="file" 
-        id="file-input" 
-        className="load-file-button"
-        onChange={(e) => stateManager.ReadTaskInputFromFile(e)}
-    />;
+const LoadFileButton = () => {
+    let fileInputRef = React.createRef<any>();
+    return  <div className="button-item">
+                <Button className="load-file-button"
+                    content="Загрузить параметры из файла" 
+                    icon="file"
+                    onClick={() => fileInputRef.current.click()}
+                />
+                <input
+                    ref={fileInputRef}
+                    type="file"
+                    hidden
+                    onChange={(e) => stateManager.ReadTaskInputFromFile(e)}
+                />
+    </div>;
+}
 
 export default LoadFileButton;
